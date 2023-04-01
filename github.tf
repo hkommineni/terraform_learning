e0cb40df94aff24ad726e0702975cb27a4cb3f7a
hkommineni <kommineniharish@live.com>
2023-03-31 21:04:46 -0500

    Update github.tf

--- github.tf
+++ github.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-    token = "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"
+    token = "Your-Key"
 }
 
 resource "github_repository" "example" {
